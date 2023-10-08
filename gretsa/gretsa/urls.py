ae8de832f8fa4dfadfb3d5c47296ffe141e1ce26
derick-droid <derrickokinda9@gmail.com>
2023-10-08 20:40:10 +0300

    room

--- gretsa/gretsa/urls.py
+++ gretsa/gretsa/urls.py
@@ -22,8 +22,12 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("home page")
 
+def room(request):
+    return HttpResponse("room")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home)
+    path("", home),
+    path("room", room)
     
 ]
